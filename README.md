561e085f860b9daaa02627b7baa37aa0f10a4eaf
Ben H <deepfreespace@gmail.com>
2022-08-17 16:04:50 -0600

    Playing with types

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # typescript-practice
+
 A repository to hold various Typescript practice projects.
+
+## Resources
+
+Primarily built using Maximilian Schwarzmüller's tutorial found [here](https://www.youtube.com/watch?v=BwuLxPH8IDs).
--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Code goes here!');
+function add(n1, n2, showResult, phrase) {
+    if (showResult) {
+        var result_1 = phrase + n1 + n2;
+        console.log(result_1, typeof result_1);
+    }
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+var result = add(number1, number2, printResult, resultPhrase);
