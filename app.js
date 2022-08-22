91e247e2eca51826f5a5300dbee7fb80110ade0b
Ben H <deepfreespace@gmail.com>
2022-08-22 16:37:02 -0600

    Function callback types

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
+function addAndHandle(n1, n2, callback) {
+    var result = n1 + n2;
+    callback(result);
+}
 printResult(add(5, 12));
 // let combineValues: Function; -> This tells TS that combineValues should store a function, but it could be ANY function -- so if we set it to be printResult, for example, we'd run into an error because printResult doesn't take two arguments like we're adding in the console.log below.
 // This line tells us that combineValues contains a function with NO parameters and then returns a number.
 // let combineValues: () => number;
-// This now tells us that combineValues should accept any functions with two parameters, each of which should be a number, and returns a number.
+// This now tells us that combineValues should accept any function with two parameters, each of which should be a number, and returns a number.
 var combineValues;
 combineValues = add;
 // combineValues = 5; -> If we don't specify a type for combineValues, Ts infers "any" -- so we could set combineValues to 5 and TS wouldn't throw any errors, but we'd hit one in runtime telling us that combineValues is not a function.
 console.log(combineValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
