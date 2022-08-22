93443cf4e50aa474019456416f4c7a9cb541915d
Ben H <deepfreespace@gmail.com>
2022-08-21 18:23:28 -0600

    Type aliases

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
+// Creates a type alias so we don't have to type out 'number | string' everywhere else in our code
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-string';
+
 // The pipe in between type assignments indicates a 'union,' i.e. whatever types we include (and we can have more than two) are acceptable values
 function combine(
-    input1: number | string,
-    input2: number | string,
+    input1: Combinable,
+    input2: Combinable,
     // Use a literal type to not only define what *type* the value must be but also exactly which values of that type are even acceptable
-    resultConversion: 'as-number' | 'as-string'
+    resultConversion: ConversionDescriptor
 ) {
     let result;
     // One downside of unions: TS can't tell what types you're including in the union, it just sees that a particular value can accept multiple types, so TS will give us an error telling us that you can't use the + operator because it doesn't know that we included a union of number and string -- just that we have a union. We have to add runtime checks like the following if we want to use unions like this:
