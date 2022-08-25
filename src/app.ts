57540f3fa75fdb54a594cbc32f42f7d18aad0324
Ben H <deepfreespace@gmail.com>
2022-08-25 17:12:55 -0600

    Typescript  arrow functions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here!
+const userName = 'Ben';
+let age = 33;
+
+const add = (a: number, b: number) => a + b;
+
+console.log(add(2, 5));
+
+const printOutput: (a: number | string) => void = (output) =>
+    console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+    button.addEventListener('click', (event) => console.log(event));
+}
+
+printOutput(add(5, 2));
