0a08efb116e0e214594c64af0166f21d7508bc7b
Ben H <deepfreespace@gmail.com>
2022-09-05 16:12:56 -0600

    Using interfaces with classes

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,26 @@
 // Simplified, an interface describes the structure of an object. It only exists in Typescript. Interfaces are essentially just used for type-checking. Whereas a class is an object factory that CAN be used to force types, you just use an interface to compare an object with the interface to ensure it matches all the required properties and methods.
-interface Person {
+interface Greetable {
     name: string;
-    age: number;
     // For methods, we don't describe the method -- just the return type and types of arguments.
     greet(phrase: string): void;
 }
 
-// We can use our interface as a type. By doing so, we can use the interface to type-check any objects with this interface as a type.
-let user1: Person;
-
-user1 = {
-    name: 'Ben',
-    age: 33,
+// While you can only inherit one CLASS, you can implement MULTIPLE interfaces
+class Person implements Greetable {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
     greet(phrase: string) {
         console.log(phrase + ' ' + this.name + '.');
-    },
-};
+    }
+}
 
+// We can use our interface as a type. By doing so, we can use the interface to type-check any objects with this interface as a type.
+let user1: Greetable;
+
+user1 = new Person('Benjamin', 33);
 user1.greet('Hi there. My name is');
+console.log(user1);
