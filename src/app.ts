18a6c5b3ceb96abe757aea08159317955bfcf47b
Ben H <deepfreespace@gmail.com>
2022-10-01 18:53:51 -0600

    Generic classes

--- src/app.ts
+++ src/app.ts
@@ -63,3 +63,33 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 console.log(extractAndConvert({ name: 'Ben' }, 'name'));
+
+// This really only works with primitive types because of the way objects are stored and referenced in JS -- hence the constraints to primitive types in the class definition.
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+
+textStorage.addItem('Ben');
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Manu');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
