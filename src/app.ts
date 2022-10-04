1a0885c8aabe1b8e78e3ef754ef887fd0bc7d71f
Ben H <deepfreespace@gmail.com>
2022-10-03 18:03:04 -0600

    Working with decorator factories

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
-// Decorators run when the class is defined, not when an instance is created
+/* // Decorators run when the class is defined, not when an instance is created
 function Logger(constructor: Function) {
     // Decorators typically use uppercase names
     console.log('Logging...');
     console.log(constructor);
+} */
+
+// This is a factory decorator
+function Logger(logString: string) {
+    return function (constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
+    };
 }
 
-@Logger
+// @Logger // This is how you call a non-factory decorator
+@Logger('Logging person...') // When you want to run a factory decorator, you need to call it with ()
 class Person {
     name = 'Ben';
 
