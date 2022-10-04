91fb30aa8c8997bdb9f04e64f3c6cb6911e498a4
Ben H <deepfreespace@gmail.com>
2022-10-03 18:17:22 -0600

    Building more useful decorators

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ function Logger(constructor: Function) {
     console.log(constructor);
 } */
 
-// This is a factory decorator
+// This is a factory decorator. Factory decorators return a decorator function, rather than being a decorator function themselves. This is useful because it allows us to pass in arguments to the decorator (which we do when we call the decorator with ())
 function Logger(logString: string) {
     return function (constructor: Function) {
         console.log(logString);
@@ -13,8 +13,21 @@ function Logger(logString: string) {
     };
 }
 
+function WithTemplate(template: string, hookId: string) {
+    // _ is a convention for unused parameters, telling TS that we don't need that parameter
+    // return function (_: Function) {
+    return function (constructor: any) {
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector('h1')!.textContent = p.name;
+        }
+    };
+}
+
 // @Logger // This is how you call a non-factory decorator
-@Logger('Logging person...') // When you want to run a factory decorator, you need to call it with ()
+@WithTemplate('<h1>My Person</h1>', 'app') // When you want to run a factory decorator, you need to call it with ()
 class Person {
     name = 'Ben';
 
